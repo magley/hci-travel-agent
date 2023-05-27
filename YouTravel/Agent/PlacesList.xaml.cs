c9c86f227b25517d26778b0a92df279787854f5f
magley <60552482+magley@users.noreply.github.com>
2023-05-27 14:49:14 +0200

    feat: Add nicer styling when no places and custom popup window
    Because styling the default MessageBox is hard/impossible.

--- YouTravel/Agent/PlacesList.xaml.cs
+++ YouTravel/Agent/PlacesList.xaml.cs
@@ -2,11 +2,13 @@
 using Microsoft.Maps.MapControl.WPF;
 using System;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using YouTravel.Model;
+using YouTravel.Shared;
 
 namespace YouTravel.Agent
 {
@@ -18,33 +20,61 @@ namespace YouTravel.Agent
 		{
 			InitializeComponent();
 			DataContext = this;
-		}
 
-		private void Window_Loaded(object sender, RoutedEventArgs e)
+			Places.CollectionChanged += OnPlaceCollectionChanged;
+        }
+
+        private void OnPlaceCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+		{
+			OnPlacesCollectionChanged();
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
 			InitDbContext();
 			InitMapsApi();
-		}
+        }
 
 		private void InitDbContext()
 		{
 			LoadPlaces();
 		}
 
-		private void LoadPlaces()
+		private void OnPlacesCollectionChanged()
+		{
+            if (Places.Count > 0)
+            {
+                txtNoPlaces.Visibility = Visibility.Collapsed;
+                lstPlaces.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtNoPlaces.Visibility = Visibility.Visible;
+                lstPlaces.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void LoadPlaces()
 		{
 			using (var ctx = new TravelContext())
 			{
                 ctx.Places.Load();
-				Places = ctx.Places.Local.ToObservableCollection();
-				lstPlaces.DataContext = Places;
+
+				Places.Clear();
+				foreach (var v in ctx.Places.Local)
+				{
+					Places.Add(v);
+                }
+
+                lstPlaces.DataContext = Places;
 
 				if (Places.Count > 0)
 				{
 					lstPlaces.SelectedIndex = 0;
-				}
+                }
 
-				PinToSelectedListItem();
+                PinToSelectedListItem();
 			}
 		}
 
@@ -125,6 +155,17 @@ namespace YouTravel.Agent
 
 		private void btnRemovePlace_Click(object sender, RoutedEventArgs e)
 		{
+			bool confirmed = false;
+            ConfirmBox confirmBox = new("Are you sure you want to delete this place?", "Delete Confirmation");
+            if (confirmBox.ShowDialog() == false)
+            {
+				confirmed = confirmBox.Result;
+			}
+			if (!confirmed)
+			{
+				return;
+			}
+
 			using (var ctx = new TravelContext())
 			{
                 Button btn = (Button)sender;
---
+++ YouTravel/Shared/ConfirmBox.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace YouTravel.Shared
+{
+    public partial class ConfirmBox : Window
+    {
+        public bool Result { get; private set; } = false;
+        public string MessageBody { get; set; }
+
+        public ConfirmBox(string messageBody, string windowTitle)
+        {
+            InitializeComponent();
+            DataContext = this;
+            MessageBody = messageBody;
+            Title = windowTitle;
+
+            Application curApp = Application.Current;
+            Window mainWindow = curApp.MainWindow;
+            this.Owner = mainWindow;
+        }
+
+        private void btnYes_Click(object sender, RoutedEventArgs e)
+        {
+            Result = true;
+            Close();
+        }
+
+        private void btnNo_Click(object sender, RoutedEventArgs e)
+        {
+            Result = false;
+            Close();
+        }
+    }
+}
