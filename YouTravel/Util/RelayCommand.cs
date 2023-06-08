cdfe640a367a42b792eb637b7c49e0471c5ff9fe
filipkontic <80402248+filipkontic@users.noreply.github.com>
2023-06-08 15:44:26 +0200

    fix: Fix shortcuts after breaking them

--- YouTravel/Util/RelayCommand.cs
+++ YouTravel/Util/RelayCommand.cs
@@ -5,8 +5,8 @@ namespace YouTravel.Util
 {
     public class RelayCommand : ICommand
     {
-        private readonly Action<object> _execute;
-        private readonly Func<object, bool>? _canExecute;
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool>? _canExecute;
 
         public event EventHandler? CanExecuteChanged
         {
@@ -14,7 +14,7 @@ namespace YouTravel.Util
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public RelayCommand(Action<object> execute, Func<object, bool>? canExecute)
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute)
         {
             _execute = execute;
             _canExecute = canExecute;
@@ -22,13 +22,12 @@ namespace YouTravel.Util
 
         public bool CanExecute(object? parameter)
         {
-            if (parameter == null || _canExecute == null) return false;
+            if (_canExecute == null) return false;
             return _canExecute(parameter);
         }
 
         public void Execute(object? parameter)
         {
-            if (parameter == null) return;
             _execute(parameter);
         }
     }
